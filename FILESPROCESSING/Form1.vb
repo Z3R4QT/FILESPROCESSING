334cf8cb2f2aa469f9780926e591c4797bf44dd5
Z3R4QT <christopher092480@gmail.com>
2025-11-04 18:23:15 +0800

    messageBox with messageError

--- FILESPROCESSING/Form1.vb
+++ FILESPROCESSING/Form1.vb
@@ -1,3 +1,21 @@
-﻿Public Class Form1
+﻿Imports System.CodeDom
+Imports System.IO
+Imports System.Linq.Expressions
 
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim filePath As String = "sample.txt"
+
+            Using Writer As New StreamWriter(filePath)
+                Writer.WriteLine("Hello this is a test files!")
+                Writer.WriteLine("Second line of text.")
+            End Using
+
+            MessageBox.Show("File written successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show("Error writing file: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
 End Class
